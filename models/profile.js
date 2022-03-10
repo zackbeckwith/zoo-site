c3cdf09353dd5c289cf4a1b0bc746acb5e49eee5
zackbeckwith <z.beckwith99@gmail.com>
2022-03-09 20:57:09 -0500

    modify models

--- models/profile.js
+++ models/profile.js
@@ -6,6 +6,9 @@ const zoosSchema = new Schema({
   name: {type: String,
     required: true},
   collectedAnimals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Animal' }],
+  owner: {
+    type: Schema.Types.ObjectId, ref: "Profile"
+  }
 })
 
 const profileSchema = new Schema({
